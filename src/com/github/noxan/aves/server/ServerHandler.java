4fdf54c7942d656b8fa4376f8cbd9d7becae9449
noxan <noxan@redmoonstudios.de>
2012-04-04 14:39:00 +0200

    define handledata method in serverhandler

--- src/com/github/noxan/aves/server/ServerHandler.java
+++ src/com/github/noxan/aves/server/ServerHandler.java
@@ -1,5 +1,7 @@
 package com.github.noxan.aves.server;
 
+import com.github.noxan.aves.net.Connection;
 
 public interface ServerHandler {
+    public void handleData(Connection connection, Object data);
 }
