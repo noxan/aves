caa8323a09223d8bd5cd87f32a0c34afb8098eff
noxan <noxan@redmoonstudios.de>
2012-04-04 11:19:05 +0200

    save server and socket reference as attribute in socketconnection

--- src/com/github/noxan/aves/net/SocketConnection.java
+++ src/com/github/noxan/aves/net/SocketConnection.java
@@ -5,6 +5,11 @@ import java.net.Socket;
 import com.github.noxan.aves.server.Server;
 
 public class SocketConnection {
+    private Server server;
+    private Socket socket;
+
     public SocketConnection(Server server, Socket socket) {
+	this.server = server;
+	this.socket = socket;
     }
 }
