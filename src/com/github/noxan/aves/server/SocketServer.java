40c701a0e1b03eb673591da7fff4eaeba73caea6
noxan <noxan@redmoonstudios.de>
2012-04-04 11:09:11 +0200

    let socketserver implement server interface

--- src/com/github/noxan/aves/server/SocketServer.java
+++ src/com/github/noxan/aves/server/SocketServer.java
@@ -2,7 +2,7 @@ package com.github.noxan.aves.server;
 
 import java.net.ServerSocket;
 
-public class SocketServer {
+public class SocketServer implements Server {
     private ServerSocket server;
 
     public SocketServer() {
