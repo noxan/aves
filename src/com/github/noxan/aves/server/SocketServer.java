a602ef7fd8e3d2341dcf0948a8d14c870281c570
noxan <noxan@redmoonstudios.de>
2012-04-04 11:09:48 +0200

    add host and port to socketserver constructor and add attributes

--- src/com/github/noxan/aves/server/SocketServer.java
+++ src/com/github/noxan/aves/server/SocketServer.java
@@ -3,8 +3,14 @@ package com.github.noxan.aves.server;
 import java.net.ServerSocket;
 
 public class SocketServer implements Server {
+    private String host;
+    private int port;
+
     private ServerSocket server;
 
-    public SocketServer() {
+    public SocketServer(String host, int port) {
+	this.host = host;
+	this.port = port;
+    }
     }
 }
