150b9a74442ece538ba35a3f3aa7d3362de5b967
noxan <noxan@redmoonstudios.de>
2012-04-04 12:48:29 +0200

    wrap in and output streams of socket in connection with string protocol streams

--- src/com/github/noxan/aves/net/SocketConnection.java
+++ src/com/github/noxan/aves/net/SocketConnection.java
@@ -1,17 +1,29 @@
 package com.github.noxan.aves.net;
 
+import java.io.IOException;
 import java.net.InetAddress;
 import java.net.Socket;
 
+import com.github.noxan.aves.protocol.string.StringInputProtocol;
+import com.github.noxan.aves.protocol.string.StringOutputProtocol;
 import com.github.noxan.aves.server.Server;
 
 public class SocketConnection implements Connection {
     private Server server;
     private Socket socket;
 
+    private StringInputProtocol in;
+    private StringOutputProtocol out;
+
     public SocketConnection(Server server, Socket socket) {
 	this.server = server;
 	this.socket = socket;
+	try {
+	    in = new StringInputProtocol(socket.getInputStream());
+	    out = new StringOutputProtocol(socket.getOutputStream());
+	} catch (IOException e) {
+	    e.printStackTrace();
+	}
     }
 
     @Override
