1a941f2d1039e63a8db08440e0ed4b81bb6ba5ae
noxan <noxan@redmoonstudios.de>
2012-04-04 14:47:00 +0200

    implement tuple container class for two generic elements

---
+++ src/com/github/noxan/aves/util/Tuple.java
@@ -0,0 +1,27 @@
+package com.github.noxan.aves.util;
+
+public class Tuple<A, B> {
+    private A first;
+    private B second;
+
+    public Tuple(A first, B second) {
+	setFirst(first);
+	setSecond(second);
+    }
+
+    public void setFirst(A first) {
+	this.first = first;
+    }
+
+    public A getFirst() {
+	return first;
+    }
+
+    public void setSecond(B second) {
+	this.second = second;
+    }
+
+    public B getSecond() {
+	return second;
+    }
+}
