1c978db6019a924fbc58fc99b468a6c16f0b771d
noxan <noxan@redmoonstudios.de>
2012-04-04 12:41:28 +0200

    implement stringinputprotocol to wrap inputstream from connection and read line based strings

--- src/com/github/noxan/aves/protocol/string/StringInputProtocol.java
+++ src/com/github/noxan/aves/protocol/string/StringInputProtocol.java
@@ -1,5 +1,19 @@
 package com.github.noxan.aves.protocol.string;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
 
 public class StringInputProtocol {
+    private BufferedReader in;
+
+    public StringInputProtocol(InputStream in) {
+	this.in = new BufferedReader(new InputStreamReader(in));
+    }
+
+    public Object read() throws IOException {
+	Object data = in.readLine();
+	return data;
+    }
 }
