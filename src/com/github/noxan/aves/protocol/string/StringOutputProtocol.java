a25c6948afe257f1fd36b72e99cd99c84421ef46
noxan <noxan@redmoonstudios.de>
2012-04-04 12:41:53 +0200

    implement stringoutputprotocol to wrap outputstream from connection and write line based strings

--- src/com/github/noxan/aves/protocol/string/StringOutputProtocol.java
+++ src/com/github/noxan/aves/protocol/string/StringOutputProtocol.java
@@ -1,5 +1,19 @@
 package com.github.noxan.aves.protocol.string;
 
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.OutputStream;
+import java.io.OutputStreamWriter;
 
 public class StringOutputProtocol {
+    private BufferedWriter out;
+
+    public StringOutputProtocol(OutputStream out) {
+	this.out = new BufferedWriter(new OutputStreamWriter(out));
+    }
+
+    public void write(Object data) throws IOException {
+	out.write(data.toString());
+	out.flush();
+    }
 }
