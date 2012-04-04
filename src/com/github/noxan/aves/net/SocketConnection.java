51d319fe709a1d56ea649778ddaec06800cc1c8c
noxan <noxan@redmoonstudios.de>
2012-04-04 11:19:45 +0200

    let socketconnection implement connection and implement get host and port methods

--- src/com/github/noxan/aves/net/SocketConnection.java
+++ src/com/github/noxan/aves/net/SocketConnection.java
@@ -1,15 +1,26 @@
 package com.github.noxan.aves.net;
 
+import java.net.InetAddress;
 import java.net.Socket;
 
 import com.github.noxan.aves.server.Server;
 
-public class SocketConnection {
+public class SocketConnection implements Connection {
     private Server server;
     private Socket socket;
 
     public SocketConnection(Server server, Socket socket) {
 	this.server = server;
 	this.socket = socket;
+    }
+
+    @Override
+    public InetAddress getHost() {
+	return socket.getInetAddress();
+    }
+
+    @Override
+    public int getPort() {
+	return socket.getPort();
     }
 }
