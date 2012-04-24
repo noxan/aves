589f70b384dddd84d74b62902110bf2f36d972a7
noxan <noxan@redmoonstudios.de>
2012-04-24 10:57:52 +0200

    implement requestUser method in inmemory storage to check if user exists and try to login

--- src/com/github/noxan/aves/auth/storage/InMemoryUsernamePasswordStorage.java
+++ src/com/github/noxan/aves/auth/storage/InMemoryUsernamePasswordStorage.java
@@ -3,6 +3,8 @@ package com.github.noxan.aves.auth.storage;
 import java.util.HashMap;
 import java.util.Map;
 
+import com.github.noxan.aves.auth.accessor.UsernamePasswordAccessor;
+
 public class InMemoryUsernamePasswordStorage {
     private Map<String, String> users;
 
@@ -12,5 +14,12 @@ public class InMemoryUsernamePasswordStorage {
 
     public void addUser(String username, String password) {
         users.put(username, password);
+    }
+
+    public String requestUser(UsernamePasswordAccessor accessor) {
+        if (accessor.checkPassword(users.get(accessor.getUsername()))) {
+            return accessor.getUsername();
+        }
+        return null;
     }
 }
