3dc3cc2ad09dacd8ce7d64e5a19562d25337b086
noxan <noxan@redmoonstudios.de>
2012-05-05 17:09:02 +0200

    remove unnecessary semicolor from serverevent enum

--- src/com/github/noxan/aves/server/ServerEvent.java
+++ src/com/github/noxan/aves/server/ServerEvent.java
@@ -6,5 +6,5 @@
 package com.github.noxan.aves.server;
 
 public enum ServerEvent {
-    DATA_READ, DATA_WRITE, CLIENT_CONNECT, CLIENT_DISCONNECT, CLIENT_LOST;
+    DATA_READ, DATA_WRITE, CLIENT_CONNECT, CLIENT_DISCONNECT, CLIENT_LOST
 }
