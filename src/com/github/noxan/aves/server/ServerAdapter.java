bbe8f84e279f5b2e03d108ad82ed8dd88de9d84e
noxan <noxan@redmoonstudios.de>
2012-05-05 16:57:22 +0200

    remove semicolons from server adapter

--- src/com/github/noxan/aves/server/ServerAdapter.java
+++ src/com/github/noxan/aves/server/ServerAdapter.java
@@ -4,6 +4,10 @@ import com.github.noxan.aves.net.Connection;
 
 public class ServerAdapter implements ServerHandler {
     @Override
+    public void readData(Connection connection, Object data) {
+    }
+
+    @Override
     public void clientConnect(Connection connection) {
     }
 
@@ -11,9 +15,7 @@ public class ServerAdapter implements ServerHandler {
     public void clientDisconnect(Connection connection) {
     }
 
+    @Override
     public void clientLost(Connection connection) {
-    };
-
-    public void readData(Connection connection, Object data) {
-    };
+    }
 }
