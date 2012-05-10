77f5150db7583804f60218f3c29e18f086f8519d
noxan <noxan@redmoonstudios.de>
2012-05-10 16:10:19 +0200

    define minimal methods for a client

--- src/com/github/noxan/aves/client/Client.java
+++ src/com/github/noxan/aves/client/Client.java
@@ -1,5 +1,11 @@
 package com.github.noxan.aves.client;
 
+import java.io.IOException;
 
 public interface Client {
+    public String getHost();
+
+    public int getPort();
+
+    public void connect() throws IOException;
 }
