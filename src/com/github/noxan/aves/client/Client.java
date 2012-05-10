a949630b317496a817463efd75fdbbe0dd23896f
noxan <noxan@redmoonstudios.de>
2012-05-10 18:58:44 +0200

    remove getHost and getPort from client interface (is in connection already)

--- src/com/github/noxan/aves/client/Client.java
+++ src/com/github/noxan/aves/client/Client.java
@@ -3,10 +3,6 @@ package com.github.noxan.aves.client;
 import java.io.IOException;
 
 public interface Client {
-    public String getHost();
-
-    public int getPort();
-
     public void connect() throws IOException;
 
     public void disconnect();
