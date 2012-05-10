73b02edf2d4f1cd242dfd2bfa19da607e69de70f
noxan <noxan@redmoonstudios.de>
2012-05-10 16:11:32 +0200

    let socketclient implement client interface

--- src/com/github/noxan/aves/client/socket/SocketClient.java
+++ src/com/github/noxan/aves/client/socket/SocketClient.java
@@ -1,4 +1,25 @@
 package com.github.noxan.aves.client.socket;
 
-public class SocketClient {
+import java.io.IOException;
+
+import com.github.noxan.aves.client.Client;
+
+public class SocketClient implements Client {
+    @Override
+    public String getHost() {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    public int getPort() {
+        // TODO Auto-generated method stub
+        return 0;
+    }
+
+    @Override
+    public void connect() throws IOException {
+        // TODO Auto-generated method stub
+
+    }
 }
