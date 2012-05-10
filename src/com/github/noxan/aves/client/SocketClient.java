1f1774eac1488e0648801c92998a20365e9d32e2
noxan <noxan@redmoonstudios.de>
2012-05-10 18:59:18 +0200

    add protocols with factory to socketclient and initial on connect

--- src/com/github/noxan/aves/client/SocketClient.java
+++ src/com/github/noxan/aves/client/SocketClient.java
@@ -5,6 +5,10 @@ import java.net.InetSocketAddress;
 import java.net.Socket;
 
 import com.github.noxan.aves.net.Connection;
+import com.github.noxan.aves.protocol.InputProtocol;
+import com.github.noxan.aves.protocol.OutputProtocol;
+import com.github.noxan.aves.protocol.ProtocolFactory;
+import com.github.noxan.aves.protocol.string.StringProtocolFactory;
 
 public class SocketClient implements Client, Connection {
     private String host;
@@ -12,14 +16,19 @@ public class SocketClient implements Client, Connection {
     private Socket socket;
     private ClientHandler handler;
 
+    private ProtocolFactory factory;
+    private InputProtocol in;
+    private OutputProtocol out;
+
     public SocketClient(ClientHandler handler) {
-        this("localhost", 1666, handler);
+        this("localhost", 1666, handler, new StringProtocolFactory());
     }
 
-    public SocketClient(String host, int port, ClientHandler handler) {
+    public SocketClient(String host, int port, ClientHandler handler, ProtocolFactory factory) {
         this.host = host;
         this.port = port;
         this.handler = handler;
+        this.factory = factory;
     }
 
     @Override
@@ -41,6 +50,8 @@ public class SocketClient implements Client, Connection {
         handler.clientConnect();
         socket = new Socket();
         socket.connect(new InetSocketAddress(host, port));
+        in = factory.createInputProtocol(socket.getInputStream());
+        out = factory.createOutputProtocol(socket.getOutputStream());
     }
 
     @Override
