f9e99c22ea9610a79576add6d2a6caa232232b7b
noxan <noxan@redmoonstudios.de>
2012-05-12 10:31:17 +0200

    create clientadapter to wrap clienthandler

---
+++ src/com/github/noxan/aves/client/ClientAdapter.java
@@ -0,0 +1,23 @@
+package com.github.noxan.aves.client;
+
+public class ClientAdapter implements ClientHandler {
+    @Override
+    public void clientConnect() {
+    }
+
+    @Override
+    public void clientDisconnect() {
+    }
+
+    @Override
+    public void readData(Object data) {
+    }
+
+    @Override
+    public void serverDisconnect() {
+    }
+
+    @Override
+    public void serverLost() {
+    }
+}
